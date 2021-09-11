f9d0904a352cbae7de7d288e592f8ad07084a4eb
engrghulamali <engrlife021@gmail.com>
2021-09-11 15:47:50 +0500

    admin logo and icon change

--- public/js/app.js
+++ public/js/app.js
@@ -50025,8 +50025,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xmp\htdocs\advance_laravel\vue-and-laravel\multiVueLaravel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xmp\htdocs\advance_laravel\vue-and-laravel\multiVueLaravel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xmp\htdocs\advance_laravel\vue-and-laravel\laravelVuePractice\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xmp\htdocs\advance_laravel\vue-and-laravel\laravelVuePractice\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
